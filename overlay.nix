cd686d63d8d9e730090a2968bbef56b0c4358d7e
Jason Cox <steel300@gmail.com>
2022-10-24 22:49:06 -0500

    fix(overlay): Make sure we call generated.nix with the right arguments

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,7 @@ final: prev:
 prev.lib.composeManyExtensions [
   (self: super: let
     origin = self.callPackage ./generated.nix {
-      inherit (self.vimUtils) buildVimPluginFrom2Nix;
+      inherit (self.vimUtils) buildNeovimPluginFrom2Nix buildVimPluginFrom2Nix;
     };
   in {
     vimExtraPlugins = final.lib.makeExtensible (_: final.lib.recurseIntoAttrs origin);
