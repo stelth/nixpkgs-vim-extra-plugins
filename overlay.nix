63f4d9cc3b986be3f00f032959e85d6d8fab0d8f
Jason Cox <steel300@gmail.com>
2022-10-24 22:51:05 -0500

    fix(overlay): Inherit buildNeovimPluginFrom2Nix from the correct place

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,8 @@ final: prev:
 prev.lib.composeManyExtensions [
   (self: super: let
     origin = self.callPackage ./generated.nix {
-      inherit (self.vimUtils) buildNeovimPluginFrom2Nix buildVimPluginFrom2Nix;
+      inherit (self.vimUtils) buildVimPluginFrom2Nix;
+      inherit (self.neovimUtils) buildNeovimPluginFrom2Nix;
     };
   in {
     vimExtraPlugins = final.lib.makeExtensible (_: final.lib.recurseIntoAttrs origin);
